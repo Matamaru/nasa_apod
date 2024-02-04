8a68638c82a47cc5320ac72429df7755cc8482fb
Matamaru <hmatamaru@outlook.de>
2024-02-04 06:26:19 +0100

    save image and text in files

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import os
 import requests
-import webbrowser
 from PIL import Image
+import datetime
+
+#create folder if not exist
+if not os.path.exists('data'):
+    os.makedirs('data')
+
+# create folder image in data if not exist
+if not os.path.exists('data/image'):
+    os.makedirs('data/image')
+
+# create folder text in data if not exist
+if not os.path.exists('data/text'):
+    os.makedirs('data/text')
 
 # Get the API key from the environment
 api_key = os.environ.get('NASA_API_KEY')
@@ -17,6 +29,7 @@ params = {
 
 # Make the request
 response = requests.get(url, params=params)
+print(response.json())
 
 # Title of the image
 img_title = response.json()[0]['title']
@@ -27,13 +40,26 @@ img_explanation = response.json()[0]['explanation']
 # URL of the image
 img_url = response.json()[0]['url']
 
+# date of download format year-month-day (20240203)
+date = datetime.datetime.now().strftime('%Y%m%d')
+
 # Save the image to a file
 img = requests.get(img_url).content
-with open('image.jpg', 'wb') as f:
+img_filename = date + '_' + img_title.replace(' ', '_') + '.jpg'
+img_path = os.path.join('data', 'image', img_filename)
+with open(img_path, 'wb') as f:
     f.write(img)
 
+# Save the text to the image
+text_filename = date + '_' + img_title.replace(' ', '_') + '.txt'
+text_path = os.path.join('data', 'text', text_filename)
+with open(text_path, 'w') as f:
+    f.write(f'Title: {img_title}\n')
+    f.write(f'Source: {img_url}\n')
+    f.write(f'Explanation: {img_explanation}')
+
 # Show the image in the default image viewer
-img = Image.open('image.jpg')
+img = Image.open(img_path)
 img.show()
 
 # Print title, source and explanation
